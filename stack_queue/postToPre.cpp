495e501053afde26e5ed28d2b8815c30a3908016
RiteshKr001 <riteshofficial6@gmail.com>
2025-05-30 23:04:37 +0530

    post fix to prefix and vice versa

---
+++ stack_queue/postToPre.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    string exp;
+    cin>>exp;
+
+    stack <string> st;
+    int i = 0;
+
+    int n = exp.size();
+
+    while (i<n)
+    {
+        char c = exp[i];
+        if(isalnum(c)){
+            string t = "";
+            t += c;
+            st.push(t);
+        }
+        else{
+            string o2 = st.top();
+            st.pop();
+            string o1 = st.top();
+            st.pop();
+
+            st.push( c + o1 +  o2);
+        }
+        i++;
+    }
+
+    cout<<st.top();
+    
+
+    return 0;
+}
